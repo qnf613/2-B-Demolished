39bf395e39ae340715de792d9ca7d59f06bd3ad0
qnf613 <gurwls613@gmail.com>
2020-10-05 23:03:42 -0500

    Basic movement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
 
-    // Update is called once per frame
-    void Update()
+    //Component
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    //Values of movement
+    private float h;
+    private float v;
+    private bool isHorizonMove;
+    
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        //Input value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        //Check button up & down
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool vUp = Input.GetButtonUp("Vertical");
+        //Check horizontal movement
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        //Movement
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;
     }
 }
