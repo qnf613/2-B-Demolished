96fc6d2061dab4056d528b6981a8016dd6c876ee
Ricarto <gurwls613@gmail.com>
2020-11-27 09:05:01 -0600

    warning before boss battle (temp)

---
+++ Assets/Scripts/BossStageAlter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStageAlter : MonoBehaviour
+{
+    AudioSource AS;
+    [SerializeField] private GameObject warningPanel;
+    [SerializeField] private AudioClip warning;
+    [SerializeField] private AudioClip bgm;
+
+    public void Awake()
+    {
+        Time.timeScale = 0;
+        AS = GetComponentInParent<AudioSource>();
+        AS.clip = warning;
+        AS.playOnAwake = true;
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            AS.clip = bgm;
+            AS.Play();
+            warningPanel.SetActive(false);
+            Time.timeScale = 1;
+            gameObject.GetComponent<BossStageAlter>().enabled = false;
+        }
+    }
+}
