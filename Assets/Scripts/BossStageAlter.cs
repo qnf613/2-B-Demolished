853cff8bca0565c77faa38a803544ffd4186ea7c
qnf613 <gurwls613@gmail.com>
2020-11-28 04:18:43 -0600

    improve boos alter + stage 3 done

--- Assets/Scripts/BossStageAlter.cs
+++ Assets/Scripts/BossStageAlter.cs
@@ -7,9 +7,11 @@ public class BossStageAlter : MonoBehaviour
     AudioSource AS;
     [SerializeField] private GameObject warningPanel;
     [SerializeField] private AudioClip bgm;
+    [SerializeField] public static bool isWarningOn;
 
     public void Awake()
     {
+        isWarningOn = true;
         //pause the game
         Time.timeScale = 0;
         AS = GetComponentInParent<AudioSource>();
@@ -18,13 +20,19 @@ public class BossStageAlter : MonoBehaviour
 
     public void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             AS.clip = bgm;
             AS.Play();
             warningPanel.SetActive(false);
             Time.timeScale = 1;
             gameObject.GetComponent<BossStageAlter>().enabled = false;
+            isWarningOn = false;
+        }
+
+        if (ApplicationManager.isRestarted)
+        {
+            isWarningOn = true;
         }
     }
 }
