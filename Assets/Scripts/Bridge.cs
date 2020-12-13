b304eacd20bd073a7d996e1aac793b8ec8141ce0
qnf613 <gurwls613@gmail.com>
2020-12-13 16:48:22 -0600

    the bridge added in stage 3, everything done before build

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    private BoxCollider2D boxcol;
+    private SpriteRenderer sRender;
+
+    private void Awake()
+    {
+        boxcol = GetComponent<BoxCollider2D>();
+        sRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Enemy.numberLeft == 0)
+        {
+            boxcol.enabled = false;
+            sRender.enabled = true;
+        }
+    }
+}
